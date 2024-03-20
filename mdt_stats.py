fec07f68869669bf5c986e5fca43fda9115cc46f
Andrew Elwell <Andrew.Elwell@pawsey.org.au>
2024-03-21 08:06:37 +1100

    Update for Neo 6.6

--- mdt_stats.py
+++ mdt_stats.py
@@ -1,84 +1,122 @@
-#!/usr/bin/python2.7
+#!/usr/bin/python3.9
 
 # Script to spit out lustre stats towards an influxdb server
-# Andrew Elwell <Andrew.Elwell@pawsey.org.au>, Sept 2017. Available under GPL2+ 
-# Hints from http://wiki.lustre.org/Lustre_Monitoring_and_Statistics_Guide 
+# Andrew Elwell <Andrew.Elwell@pawsey.org.au>, Sept 2017. Available under GPL2+
+# Hints from http://wiki.lustre.org/Lustre_Monitoring_and_Statistics_Guide
 
-import urllib2
-import time
-import os
-import fnmatch
+import requests    # makes life much easier with newer neo
+import time        # changelog_users doesn't have snapshot time
 import subprocess
-
+import re
+import os
+import socket
 import daemon
+
+
+debug = True
 
 # which nutter enabled auth on influx
 username = 'FIXME'
 password = 'FIXME'
-url = "https://influx.example.org:8086"
-pw = urllib2.HTTPPasswordMgr()
-pw.add_password("InfluxDB", url, username, password)
-handler = urllib2.HTTPBasicAuthHandler(pw)
-opener = urllib2.build_opener(handler)
-urllib2.install_opener(opener)
+url = 'https://influx.example.com:8086'
 
+hostname = socket.gethostname()
 
-
-# While I should really use 'lctl get_param', grabbing the source directly saves an extra subprocess
-
-def getrole():
-    # are we running on an MDT?
-    mdslist = os.listdir('/proc/fs/lustre/mdt/')
-    if len(mdslist) == 1:
-        mds = mdslist[0]
-        (fs,mdt) = mds.split('-',2)
-    return (mds,fs,mdt)
-
-def grabbit(info):
-    (mds,fs,mdt) = info
-    post = ""
-    with open(('/proc/fs/lustre/mdt/%s/md_stats' % mds), 'r') as f:
-        for line in f:
-            k,v,null = line.split(None,2)
-            if k == "snapshot_time":
-                ts=int(float(v)*1000000)
+def grabbit():
+    fs=''
+    mdt=''
+    ts=0
+    post = ''
+    measurement=''
+    fields = []
+    # Do we have one or more MDTs mounted?
+    mdtstats = subprocess.check_output(['lctl','get_param', 'mdt.*.md_stats']).splitlines()
+    tmp = ''
+    prefix = ''
+    for line in mdtstats:
+        line = line.decode('UTF-8')
+        if '=' in line:
+            x = re.search('mdt.(.*).md_stats', str(line))
+            fs,mdt = x.group(1).split("-")
+            tmp =f'metadata,fs={fs},mdt={mdt},host={hostname}'
+            if fields == []:
+               prefix = tmp
             else:
-               post += 'metadata,fs={3},mdt={4},mds={5} {0}={1}i {2}\n'.format(k,v,ts,fs,mdt,mds)
-    with open(('/proc/fs/lustre/mdd/%s/changelog_users' % mds), 'r') as f:
-       tmp = f.read().split()
-       # we can cheat here as they have the same format - 3rd item in list is current changelog count, and then
-       # from the 6th item on we get changelog id / position to pull into a dict
-       head = int(tmp[2])
-       clog = dict(zip(tmp[5:][0::2], tmp[5:][1::2]))
-       post += 'changelog,fs={2},mdt={3},mds={4} head={0}i {1}\n'.format(head,ts,fs,mdt,mds)
-       for cl,count in clog.items():
-           post += 'changelog,fs={3},mdt={4},mds={5} {0}={1}i {2}\n'.format(cl,count,ts,fs,mdt,mds)
+                #print('NEW BLOCK', fields, prefix)
+                measurement += '{} {} {}\n'.format(prefix,','.join(fields), ts)
+                fields = []
+                prefix = tmp
+                tmp = ''
+        else:
+          key,val,null = line.split(None,2)
+          if key == "snapshot_time":
+            ts=val.replace('.','') # mmmm. nanosecond accuracy on metrics.
+          elif '_time' in key:
+              continue
+          else:
+              fields.append('{}={}'.format(key,int(val))) #
+    if fields != []:
+       measurement += '{} {} {}\n'.format(prefix,','.join(fields), ts)
 
-    ts = int(time.time()*1000000)
+    # are there any changelogs?
+    tmp = ''
+    clstats = subprocess.check_output(['lctl','get_param', 'mdd.*.changelog_users']).splitlines()
+    ts = int(time.time()*1000000000)
+    for line in clstats:
+        line = line.decode('UTF-8')
+        if 'users=' in line:
+           if tmp  != '': # we've come to a new header
+               if fields != []:
+                   measurement += '{} {} {}\n'.format(prefix,','.join(fields), ts)
+           x = re.search('mdd.(.*).changelog_users', line)
+           fs,mdt = x.group(1).split("-")
+           fields = []
+        elif 'current_index' in line:
+            fields.append('head={}'.format(line.split()[1]))
+        elif 'ID' in line:
+            prefix = 'changelog,fs={},mdt={}'.format(fs,mdt)
+            tmp = prefix
+        else:
+          key,val,idle = line.split()
+          fields.append('{}={}'.format(key,val))
+    if fields != []:
+       measurement += '{} {} {}\n'.format(prefix,','.join(fields), ts)
+
+
+    # total files & free (on MDT)
     mdtstats = subprocess.check_output(["lctl","get_param", "osd-*.*MDT*.files*"]).splitlines()
+    ts = int(time.time()*1000000000)
     for stat in mdtstats:
-        keys,val = stat.split("=")
-        _, mds, metric = keys.split(".", 2)
-        fs,mdt = mds.split("-")
+        stat = stat.decode('UTF-8')
+        keys,val = stat.split('=')
+        _, mds, metric = keys.split('.', 2)
+        fs,mdt = mds.split('-')
 
-        post += 'usage,fs={0},mdt={1},mds={2} {3}={4}i {5}\n'.format(fs,mdt,mds,metric,val,ts)
- 
-    post=post.encode('ascii')
-    print post
-    p = urllib2.urlopen(url + '/write?db=lustre&precision=u',post)
-    print(p.getcode())
+        measurement += 'usage,fs={},mdt={} {}={} {}\n'.format(fs,mdt,metric,val,ts)
+        # yes this sucks. I should really have one measurement with total,free,used timestamp
 
-#foo = getrole()
-#grabbit(foo)
+    # we may as well grab loadavg at the same time
+    load1,load5,load15 = os.getloadavg()
+    measurement += f'loadavg,fs={fs},host={hostname} load1={load1},load5={load5},load15={load15} {ts}\n'
 
 
-with daemon.DaemonContext():
-  foo = getrole()
-  while True:
-    try:
-      grabbit(foo)
-    except:
-      # import sys
-      # sys.exit("Whoa, that went a bit Pete Tong!")
-      time.sleep(30)
-    time.sleep(10)
+
+    if debug:
+       print(measurement)
+    return measurement
+
+if debug:
+    post = grabbit()
+    p = requests.post(url+'/write?db=lustre',data=post, auth=(username, password))
+    print(p.status_code)
+else:
+    with daemon.DaemonContext():
+      while True:
+        try:
+          post = grabbit()
+          p = requests.post(url+'/write?db=lustre',data=post, auth=(username, password))
+        except:
+          import sys
+          sys.exit("Whoa, that went a bit Pete Tong!")
+        time.sleep(10)
+
--- ost_stats.py
+++ ost_stats.py
@@ -1,10 +1,12 @@
-#!/usr/bin/python
+#!/usr/bin/python3.9
 
 # Script to spit out lustre stats towards an influxdb server
 # Andrew Elwell <Andrew.Elwell@pawsey.org.au>, Sept 2017. Available under GPL2+
 # Hints from http://wiki.lustre.org/Lustre_Monitoring_and_Statistics_Guide
 
-import urllib2
+import requests
+import os
+import socket
 import time
 import subprocess
 import daemon
@@ -12,57 +14,76 @@ import daemon
 # which nutter enabled auth on influx
 username = 'FIXME'
 password = 'FIXME'
-url = "https://influx.example.com:8086"
-pw = urllib2.HTTPPasswordMgr()
-pw.add_password("InfluxDB", url, username, password)
-handler = urllib2.HTTPBasicAuthHandler(pw)
-opener = urllib2.build_opener(handler)
-urllib2.install_opener(opener)
+url = 'https://influx.example.com:8086'
 
+hostname = socket.gethostname()
 
+debug = True
 
 def grabbit():
-    post = ""
+    post = ''
 
     # Overall OST space
-    ts = int(time.time()*1000000)
-    stats = subprocess.check_output(["lctl","get_param", "obdfilter.*OST*.kbytes*"]).splitlines()
+    ts = int(time.time()*1000000000)
+    stats = subprocess.check_output(["lctl","get_param", "obdfilter.*OST*.kbytes*"]).decode('UTF-8').splitlines()
+    space = {}
     for stat in stats:
         keys,val = stat.split("=")
         _, OST, metric = keys.split(".", 2)
         fs,ost = OST.split("-")
-        post += 'usage,fs={0},ost={1} {3}={4}i {5}\n'.format(fs,ost,OST,metric,val,ts)
+        space[metric] = val
+    foo = ','.join([f'{key}={value}' for key, value in space.items()])
+    post += f'usage,fs={fs},ost={ost} {foo} {ts}\n'
 
     # Operations per OST. Read and write data is particularly interesting
     # returns multivalue with sample timestamp
-    stats = subprocess.check_output(["lctl","get_param", "obdfilter.*.stats"]).splitlines()
+    stats = subprocess.check_output(["lctl","get_param", "obdfilter.*.stats"]).decode('UTF-8').splitlines()
+    fields = []
     for line in stats:
         if line.endswith("stats="):
              fs,ost = line.split(".", 2)[1].split("-")
+             tmp = f'usage,fs={fs},ost={ost}'
+             if fields == []:
+                 prefix = tmp
+             else:
+                 post += f'{prefix} {",".join(fields)} {ts}\n'
+                 fields = []
+                 prefix = tmp
+                 tmp = ''
         elif "_bytes" in line:
-            k,count,null,null,min_size,max_size,sum_bytes = line.split()
-            post += 'usage,fs={3},ost={4} {0}={1}i {2}\n'.format(k,sum_bytes,ts,fs,ost)
+            k,count,null,null,min_size,max_size,sum_bytes,sum_sqared = line.split()
+            fields.append(f'{k}={sum_bytes}')
         else:
              k,v,null = line.split(None,2)
              if k == "snapshot_time":
-                 ts=int(float(v)*1000000)
-             else:
-                 post += 'usage,fs={3},ost={4} {0}={1}i {2}\n'.format(k,v,ts,fs,ost)
+                 ts=v.replace('.','')
+             elif '_time' in k:
+                 continue
+             #else:
+             #    print('IGNORED', line)
+    post += f'{prefix} {",".join(fields)} {ts}\n'
+
+   # we may as well grab loadavg at the same time
+    load1,load5,load15 = os.getloadavg()
+    post += f'loadavg,fs={fs},host={hostname} load1={load1},load5={load5},load15={load15} {ts}\n'
 
 
-    post=post.encode('ascii')
-    #print post
-    p = urllib2.urlopen(url + '/write?db=lustre&precision=u',post)
-    print(p.getcode())
-
-#grabbit()
+    #print(post)
+    p = requests.post(url+'/write?db=lustre',data=post, auth=(username, password))
+    #print(p.status_code, p.text)
 
 
-with daemon.DaemonContext():
-  while True:
-    try:
-      grabbit()
-    except:
-      import sys
-      sys.exit("Whoa, that went a bit Pete Tong!")
-    time.sleep(10)
+
+
+if debug:
+    grabbit()
+
+else:
+    with daemon.DaemonContext():
+        while True:
+            try:
+                grabbit()
+            except:
+                import sys
+                sys.exit("Whoa, that went a bit Pete Tong!")
+            time.sleep(10)
